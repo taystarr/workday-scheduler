dee3330bc0cb60b1a32dac81f1650b4dd8de99a8
Tay Starr <tstarrroberts@gmail.com>
2022-03-25 04:04:57 -0400

    add past present future properties

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,29 @@
 var currentDateEl = document.querySelector("#currentDay");
+var saveButton = document.querySelector("#saveBtn");
 
 // display current date
 var rightNow = moment().format("MMMM Do, YYYY");
 currentDateEl.textContent = rightNow;
+
+// connect current time to text boxes
+var timeNow = moment().format("HH");
+console.log(timeNow);
+
+var hourStatus = function() {
+
+    $(".time-block").each(function() {
+        var timeBlock = $(this).attr("id");
+
+        if (timeNow > timeBlock) {
+            $(this).find(".task").addClass("past");
+        } else if (timeNow < timeBlock) {
+            $(this).find(".task").addClass("future");
+        } else {
+            $(this).find(".task").addClass("present");
+        }
+    });
+};
+
+hourStatus();
+
 
